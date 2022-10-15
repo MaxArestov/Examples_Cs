3cd95b05e1008bc88c5624e5d3c77e55dead6403
maxim arestov <aresmax1993@gmail.com>
2022-10-15 15:41:13 +0300

    added random to sum

--- Example_03_Sum/Program.cs
+++ Example_03_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 8;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 151);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 15);
+Console.WriteLine(numberB);
 int Sum = numberA + numberB;
 Console.WriteLine(Sum);
