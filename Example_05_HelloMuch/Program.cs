dfe6b4a6997e34f0b565387effc15fa2bb55c7bb
maxim arestov <aresmax1993@gmail.com>
2022-10-15 15:53:17 +0300

    Added Example 05 HelloMuch

---
+++ Example_05_HelloMuch/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "максим")
+{
+    Console.WriteLine("Приветствую тебя, дружище!");
+}
+else
+{
+    Console.Write("Здравствуй,");
+    Console.WriteLine(username);
+}
